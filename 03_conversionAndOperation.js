3e2fa2b20fed19687a1bfd5f856e9ac31d0f8264
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-01-28 19:07:58 +0530

    Add files via upload

---
+++ 03_conversionAndOperation.js
@@ -0,0 +1,57 @@
+// let score = 33;
+
+// console.log(typeof score); //number
+
+// let score = "33abc";
+
+// console.log(typeof score); //string
+// console.log(score); //33abc
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN
+
+// let score = null;
+
+// console.log(typeof score); //object
+// console.log(score); //null
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //0
+
+// let score = undefined;
+
+// console.log(typeof score); //undefined
+// console.log(score); //undefined
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //Nan
+
+// let score = true;
+
+// console.log(typeof score); //boolean
+// console.log(score); //true
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //1
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1 , false => 0
+//  " " => false
+// "pratham" => true
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); //true
+// console.log(typeof booleanIsLoggedIn); //boolean
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber); //33
+console.log(typeof stringNumber); //string
