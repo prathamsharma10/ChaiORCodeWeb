90cb1521a6f08e7fb34d1504d52fdeab48811522
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-01-28 19:07:46 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3); //ReferenceError: alert is not defined,we are using nodejs not browser
+
+//code readability should be high
+
+let name = "pratham"; //string
+let age = 22; //number
+let isLoggedIn = true; //boolean
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+
+// null => stand alone value
+
+// undefined
+
+// symbol => unique
+
+// object
+
+// console.log(typeof name);string
+// console.log(typeof age);number
+// console.log(typeof state);object
+// console.log(typeof undefined);undefined
